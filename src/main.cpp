5d25136d44a1647981b28f9e9e64f6ce92a3cda0
Mostafa Mohamed <mostafadalegend3@gmail.com>
2026-07-09 05:15:13 +0300

    test cases

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
-
-
+#include "Hospital.h"
+#include "Patient.h"
+#include "Doctor.h"
+#include "Appointment.h"
 int main()
 {
+  Hospital hospital;
+  Patient *Mostafa = new Patient("223344", "Mostafa Mohammed", 20, "0123456789");
+  Doctor *SigmaDoctor = new Doctor("123456", "Sigma Doctor", "Heart Surgical");
+  Patient *Yehia = new Patient("442133", "Yehia Mohammed", 20, "01234567449");
+  hospital.registerPatient(Mostafa);
+  hospital.registerDoctor(SigmaDoctor);
+  hospital.bookAppointment(Mostafa->getID(), SigmaDoctor->getID(), "7/9/2026", "10:01 AM", "normal");
+  hospital.registerPatient(Yehia);
+  hospital.bookAppointment(Yehia->getID(), SigmaDoctor->getID(), "7/10/2026", "12:44 PM", "urgent");
+  hospital.showAllAppointments();
+  cout << endl
+       << endl;
+  hospital.showDoctorSchedule(SigmaDoctor->getID());
+  hospital.cancelAppointment("APT1");
+  cout << endl
+       << endl;
+  hospital.showAllAppointments();
+  cout << endl
+       << endl;
+  hospital.showDoctorSchedule(SigmaDoctor->getID());
+  delete Mostafa;
+  delete Yehia;
+  delete SigmaDoctor;
   return 0;
 }
+
+// bookAppointment(string patientID, string doctorID, string date, string time, string priority)
